1d90e8220eb07e7140f001dcffaae3e5bd9f5a7f
Santiago Martinez <ssantiago.mc@outlook.com>
2019-10-30 22:03:02 -0500

    Gráficas de datos

--- prueba.py
+++ prueba.py
@@ -1,5 +1,6 @@
 
 import numpy as np;
+import matplotlib.pyplot as plt;
 import io;
 
 
@@ -18,15 +19,61 @@ class suavizado():
             clase = int(lineas[l].split(',')[self.numAtributos]);
             datLinea = lineas[l].split(',')[0:self.numAtributos];
             datLinea = [float(i) for i in datLinea];
-            self.data[clase] = np.vstack([self.data[clase], datLinea]);
 
+            if not self.data[clase].any():
+                self.data[clase] = np.array(datLinea);
+            else:
+                self.data[clase] = np.vstack([self.data[clase], datLinea]);
 
-        print(len(self.data[0]));
         archivoTexto.close();
+
+    def darDatosEntrada(self, clase=-1):
+        if clase == -1:
+            return self.data;
+        else:
+            return self.data[clase];
+
+    def darDatosSuavizados(self, clase=-1):
+        datSuavizado = self.data;
+        for cl,dat in enumerate(self.data):
+            numF = dat.shape[0];
+            numC = dat.shape[1];
+            for c in range(numC):
+                for f in range(numF):
+                    cont, sum = 1, 0;
+                    if f-1 > 0:
+                        sum += dat[f-1, c];
+                        cont += 1;
+
+                    sum += dat[f, c];
+
+                    if f+1 < numF:
+                        sum += dat[f+1, c];
+                        cont += 1;
+
+                    dat[f, c] = sum/cont;
+
+            datSuavizado[cl] = dat;
+        if clase == -1:
+            return datSuavizado;
+        else:
+            return datSuavizado[clase]
+
+
+    def graficar(self, datos, atributos, ruta):
+        plt.title("Dispersión de datos");
+        for cl,dat in enumerate(datos):
+            plt.scatter(dat[atributos[0]], dat[atributos[1]], label = 'Clase {}'.format(cl));
+
+        plt.legend(loc=0);
+        plt.savefig(ruta);
+        plt.close();
 
 
     def estado(self):
-        print('El archivo {} se inicializó con las siguientes características: \nNúmero Atributos: {} \nNúmero Clases {}'.format(self.ruta, self.numAtributos, self.numClases));
+        print(self.data);
 
 
 app = suavizado("seg-data.txt");
+app.graficar(app.darDatosEntrada(), [2, 3], "prueba.jpg");
+app.graficar(app.darDatosSuavizados(), [2, 3], "pruebaSua.jpg");
